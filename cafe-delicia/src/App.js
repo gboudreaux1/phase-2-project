48efd151778e86f735f66e8530a09b041d43258b
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-08-02 10:25:15 -0400

    added menu

--- cafe-delicia/src/App.js
+++ cafe-delicia/src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Route, Switch } from "react-router-dom"
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Menu from './components/Menu';
 
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:3000/coffee")
+    .then(resp => resp.json())
+    .then(data => setCoffee(data))
+  }, [])
+
+  const [coffee, setCoffee] = useState([])
+
   return (
     <div className="App">
-        {/* <NavBar />
+        <NavBar />
         <Switch>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/menu">
-            <Menu />
+        <Route path="/menu">
+            <Menu coffeeList = { coffee }/>
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      */}
     </div>
   );
 }
---
+++ cafe-delicia/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Home() {
+
+
+
+    return(
+        <div>
+            <h1>Cafe Delicia</h1>
+            <p>amazingly delicious or whatever</p>
+
+        </div>
+    )
+}
+
+export default Home;
