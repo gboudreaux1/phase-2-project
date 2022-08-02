998a5c5fde7cf7ecd7bc80a06dc0150c9ecbc8f2
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-08-02 13:50:49 -0400

    added buttons

--- cafe-delicia/src/components/Home.js
+++ cafe-delicia/src/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
-
+// import { image } from '../components'
 
 function Home() {
 
+    // const myStyle={
+    //     backgroundImage: `url (${image})`,
+    //     height:'100vh',
+    //     marginTop:'-70px',
+    //     fontSize:'50px',
+    //     backgroundSize: 'cover',
+    //     backgroundRepeat: 'no-repeat',
+    // };
 
 
     return(
         <div>
             <h1>Cafe Delicia</h1>
             <p>amazingly delicious or whatever</p>
-
         </div>
     )
 }
